31df000995f67354050a4999f30e67152c57cf33
Gautham Prabhu M <71874814+GauthamPrabhuM@users.noreply.github.com>
2021-01-23 10:31:13 +0530

    Create NeuralNetwork.py

---
+++ NeuralNetwork.py
@@ -0,0 +1,26 @@
+for epoch in range(20000):
+    inputs = feature_set
+
+    # feedforward step1
+    XW = np.dot(feature_set, weights) + bias
+
+    #feedforward step2
+    z = sigmoid(XW)
+
+
+    # backpropagation step 1
+    error = z - labels
+
+    print(error.sum())
+
+    # backpropagation step 2
+    dcost_dpred = error
+    dpred_dz = sigmoid_der(z)
+
+    z_delta = dcost_dpred * dpred_dz
+
+    inputs = feature_set.T
+    weights -= lr * np.dot(inputs, z_delta)
+
+    for num in z_delta:
+        bias -= lr * num
